cc0e10c7971925b29bba47a1daeae23cb5cecc34
Gabriel Xavier Silva <bielsaar@gmail.com>
2022-01-28 17:56:38 -0300

    Percebi que não precisava dos anteriores pra ajustar
    Retirado tutoriais anteriores;
    
    Feito um arquivo a mais para poder rodar ambos os servidores ao mesmo tempo.

--- src/main/java/com/xavier/workrabbit/ReceiveLogsDirect.java
+++ src/main/java/com/xavier/workrabbit/ReceiveLogsDirect.java
@@ -1,12 +1,10 @@
 package com.xavier.workrabbit;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.DeliverCallback;
+import com.rabbitmq.client.*;
 
-public class ReceiveLogs {
-    private static final String EXCHANGE_NAME = "logs";
+public class ReceiveLogsDirect {
+
+    private static final String EXCHANGE_NAME = "direct_logs";
 
     public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
@@ -14,15 +12,23 @@ public class ReceiveLogs {
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
 
-        channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+        channel.exchangeDeclare(EXCHANGE_NAME, "direct");
         String queueName = channel.queueDeclare().getQueue();
-        channel.queueBind(queueName, EXCHANGE_NAME, "");
 
+        if (argv.length < 1) {
+            System.err.println("Usage: ReceiveLogsDirect [info] [warning] [error]");
+            System.exit(1);
+        }
+
+        for (String severity : argv) {
+            channel.queueBind(queueName, EXCHANGE_NAME, severity);
+        }
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), "UTF-8");
-            System.out.println(" [x] Received '" + message + "'");
+            System.out.println(" [x] Received '" +
+                    delivery.getEnvelope().getRoutingKey() + "':'" + message + "'");
         };
         channel.basicConsume(queueName, true, deliverCallback, consumerTag -> { });
     }
---
+++ src/main/java/com/xavier/workrabbit/ReceiveLogsDirectError.java
@@ -0,0 +1,38 @@
+package com.xavier.workrabbit;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class ReceiveLogsDirectError {
+
+    private static final String EXCHANGE_NAME = "direct_logs";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+        String queueName = channel.queueDeclare().getQueue();
+
+        if (argv.length < 1) {
+            System.err.println("Usage: ReceiveLogsDirect [info] [warning] [error]");
+            System.exit(1);
+        }
+
+        for (String severity : argv) {
+            channel.queueBind(queueName, EXCHANGE_NAME, severity);
+        }
+        System.out.println(" [*] Esperando as mensagens, Gabriel. Pra sair, aperta CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Recebido: '" +
+                    delivery.getEnvelope().getRoutingKey() + "':'" + message + "'");
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> { });
+    }
+}
